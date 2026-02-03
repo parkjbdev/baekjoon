00174925020502394972d41649906f3ffb0a52a7
Park JongBeum <dev@parkjb.com>
2026-02-03 16:20:13 +0900

    solve 10171: 고양이 (https://www.acmicpc.net/problem/10171)

---
+++ b5p10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    string str =
+            "\\    /\\\n"
+            " )  ( ')\n"
+            "(  /  )\n"
+            " \\(__)|";
+    cout << str << endl;
+
+    return 0;
+}
