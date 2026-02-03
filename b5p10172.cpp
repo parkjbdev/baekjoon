c587a712cec59f91c63624c1a02065edfbccf507
Park JongBeum <dev@parkjb.com>
2026-02-03 16:20:54 +0900

    solve 10172: 개 (https://www.acmicpc.net/problem/10172)

---
+++ b5p10172.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    string s = "|\\_/|\n"
+            "|q p|   /}\n"
+            "( 0 )\"\"\"\\\n"
+            "|\"^\"`    |\n"
+            "||_/=\\\\__|";
+    cout << s << endl;
+
+    return 0;
+}
