971cd346c6a192c91dafc28623f3bad44e1be17b
Park JongBeum <dev@parkjb.com>
2026-02-03 16:18:34 +0900

    solve 11382: 꼬마 정민 (https://www.acmicpc.net/problem/11382)

---
+++ b5p11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    unsigned long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+
+    return 0;
+}
