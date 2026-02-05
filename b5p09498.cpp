4edd2ba9d3021c75a51b0b94e676d69fefcbe39e
Park JongBeum <dev@parkjb.com>
2026-02-05 22:19:44 +0900

    solve 9498: 시험 성적 (https://www.acmicpc.net/problem/9498)

---
+++ b5p09498.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int score;
+    cin >> score;
+    cout << "FFFFFFDCBAA"[score/10];
+
+    return 0;
+}
