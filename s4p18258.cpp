e904ecc45239d140d829306d00f8684f4245e7b3
Park JongBeum <dev@parkjb.com>
2026-02-05 23:24:05 +0900

    solve 18258: 큐 2 (https://www.acmicpc.net/problem/18258)

---
+++ s4p18258.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+
+    for (int i = 0; i < n; i++) {
+        string cmd;
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int num;
+            cin >> num;
+            q.push(num);
+        } else if (cmd == "pop") {
+            if (q.empty()) cout << "-1" << "\n";
+            else {
+                cout << q.front() << "\n";
+                q.pop();
+            }
+        } else if (cmd == "empty") {
+            cout << q.empty() << "\n";
+        } else if (cmd == "size") {
+            cout << q.size() << "\n";
+        } else if (cmd == "front") {
+            if (q.empty()) cout << "-1" << "\n";
+            else cout << q.front() << "\n";
+        } else if (cmd == "back") {
+            if (q.empty()) cout << "-1" << "\n";
+            else cout << q.back() << "\n";
+        }
+    }
+
+    return 0;
+}
