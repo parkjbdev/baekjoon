fe1f092dcd42f010dc583a69a972144e9e443e55
Park JongBeum <dev@parkjb.com>
2026-02-05 21:25:40 +0900

    solve 15649: N과 M (1) (https://www.acmicpc.net/problem/15649)

---
+++ s3p15649.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> visit;
+vector<int> trace;
+
+void picker(int n, int m) {
+    if (m == 0) {
+        for (int t: trace) {
+            cout << t << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (visit[i - 1]) continue;
+
+        visit[i - 1] = true;
+        trace.push_back(i);
+        picker(n, m - 1);
+        trace.pop_back();
+        visit[i - 1] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    visit = vector<bool>(n);
+    trace = vector<int>();
+    picker(n, m);
+
+    return 0;
+}
