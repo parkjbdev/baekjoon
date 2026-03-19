b8e7afa65429df6f4e74ade379a13955a0607669
Park JongBeum <dev@parkjb.com>
2026-03-19 14:50:07 +0900

    solve 11279: 최대 힙 (https://www.acmicpc.net/problem/11279)

---
+++ s2p11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    priority_queue<int> pq;
+
+    for (int i = 0; i < N; ++i) {
+        int input;
+        cin >> input;
+
+        if (input == 0) {
+            if (pq.empty()) cout << 0 << "\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(input);
+        }
+    }
+
+    return 0;
+}
